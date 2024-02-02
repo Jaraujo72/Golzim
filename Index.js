a68c26532fd94a531229e5672daf3c081a414ceb
Jose <augustoaraujo88@gmail.com>
2024-02-02 07:34:19 -0300

    inserção

---
+++ Index.js
@@ -0,0 +1,14 @@
+
+function realParaDolar(real,cotacao){
+    return  real * cotacao;
+
+}
+var r =100;
+var e = 4.5;
+
+document.getElementById("texto").innerHTML = 
+realParaDolar(r,e);
+
+function alertahellou(){
+    alert("hi hi hi");
+}
