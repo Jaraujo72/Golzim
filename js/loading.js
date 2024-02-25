865f330da1e377c0a2b821dc3f65015f0343bc56
Jose <augustoaraujo88@gmail.com>
2024-02-25 20:10:37 -0300

    Ajustes pagina home

--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,6 @@
 function showLoading() {
     const div = document.createElement('div');
-    div.classList.add("loading", "centralize");
+    div.classList.add("loading", "topPaginaLoading");
     const label = document.createElement('label');
     label.innerText = "Carregando...";
     div.appendChild(label);
